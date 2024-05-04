1f5e8cb0a2223aaf137c7e173937e55474b3b62d
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2024-05-04 16:58:49 +0300

    run script improved

--- _configs/run.sh
+++ _configs/run.sh
@@ -18,10 +18,10 @@ for job in $(echo $config | jq -r '.jobs[] | @base64'); do
 
     # Extract the job parameters
     id=$(echo $job | jq -r '.id')
-    time_expansions=$(echo $job | jq -r '.time_expansions')
-    dispatch_threshold=$(echo $job | jq -r '.dispatch_threshold')
-    subtree_focus_threshold=$(echo $job | jq -r '.subtree_focus_threshold')
-    dispatch_frontier_size=$(echo $job | jq -r '.dispatch_frontier_size')
+    param1=$(echo $job | jq -r '.param1')
+    param2=$(echo $job | jq -r '.param2')
+    param3=$(echo $job | jq -r '.param3')
+    param4=$(echo $job | jq -r '.param4')
 
     echo "Cloning repository into directory $id"
     git clone $repository_url $id
@@ -29,8 +29,8 @@ for job in $(echo $config | jq -r '.jobs[] | @base64'); do
     echo "Changing directory to $id/_configs"
     cd $id/_configs
 
-    echo "Running Python script with parameters --dispatch-frontier-size=$dispatch_frontier_size --subtree-focus-threshold=$subtree_focus_threshold --time-expansions=$time_expansions --dispatch-threshold=$dispatch_threshold "
-    python3 $python_script --dispatch-frontier-size $dispatch_frontier_size --subtree-focus-threshold $subtree_focus_threshold --time-expansions $time_expansions --dispatch-threshold $dispatch_threshold
+    echo "Running Python script with parameters --param1=$param1 --param2=$param2 --param3=$param3 --param4=$param4"
+    python3 $python_script --param1 $param1 --param2 $param2 --param3 $param3 --param4 $param4
 
     echo "Going one level up from $id/_configs"
     cd ..
@@ -40,7 +40,7 @@ for job in $(echo $config | jq -r '.jobs[] | @base64'); do
         output=$(sbatch sbatch.sbatch)
         job_id=$(echo $output | grep -oP '(?<=Submitted batch job )\d+')
         job_ids[$id]=$job_id
-        final_parameters[$id]="$time_expansions,$subtree_focus_threshold,$dispatch_threshold,$dispatch_frontier_size"
+        final_parameters[$id]="$param1,$param2,$param3,$param4"
         echo "$id - $job_id"
     else
         echo "sbatch.sbatch not found in directory $id"
@@ -51,7 +51,7 @@ for job in $(echo $config | jq -r '.jobs[] | @base64'); do
 done
 
 echo "Script execution completed."
-echo "id,batch,EPS,subtree_focus_threshold,dispatch_threshold,dispatch_frontier_size"
+echo "id,batch,param1,param2,param3,param4"
 for id in ${!job_ids[@]}; do
     echo "$id,${job_ids[$id]},${final_parameters[$id]}"
 done
