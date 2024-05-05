4e0e95b456113c1e6a29a18d91b8603893d08fe5
andrey <andrey.lavrinenko.ai.phd@gmail.com>
2024-05-06 02:18:56 +0300

    knn, ncc, clustering work locally, logs and csv are seen in text files locally

--- experiments/knn.py
+++ experiments/knn.py
@@ -36,7 +36,8 @@ def run_knn_experiment(dataset_name, k, metric, gamma):
         {"dataset_name": dataset_name, "k": k, "metric": metric, "gamma": gamma})
 
     X_train, y_train, X_test, y_test = UCR_UEA_datasets().load_dataset(dataset_name)
-
+    X_train = X_train.reshape(X_train.shape[0], -1)
+    X_test = X_test.reshape(X_test.shape[0], -1)
     model = None
 
     if metric == 'euclidean':
--- experiments/ncc.py
+++ experiments/ncc.py
@@ -36,6 +36,8 @@ def run_ncc_experiment(dataset_name, metric='euclidean', gamma=None):
         {"dataset_name": dataset_name, "metric": metric, "gamma": gamma})
 
     X_train, y_train, X_test, y_test = UCR_UEA_datasets().load_dataset(dataset_name)
+    X_train = X_train.reshape(X_train.shape[0], -1)
+    X_test = X_test.reshape(X_test.shape[0], -1)
 
     model = None
 
