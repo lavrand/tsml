4585fe259413fbd87f716a3c33fa3b45ef963e6b
andreyl <andreyl@post.bgu.ac.il>
2025-01-20 14:15:25 +0200

    26 missing datasets

--- experiments/prepare_batches.py
+++ experiments/prepare_batches.py
@@ -1,31 +1,68 @@
 import os
 import numpy as np
 
-datasets = ['Adiac', 'ArrowHead', 'Beef', 'BeetleFly', 'BirdChicken', 'Car', 'CBF', 'ChlorineConcentration',
-            'CinCECGTorso', 'Coffee', 'Computers', 'CricketX', 'CricketY', 'CricketZ', 'DiatomSizeReduction',
-            'DistalPhalanxOutlineCorrect', 'DistalPhalanxOutlineAgeGroup', 'DistalPhalanxTW', 'Earthquakes',
-            'ECG200', 'ECG5000', 'ECGFiveDays', 'ElectricDevices', 'FaceAll', 'FaceFour', 'FacesUCR', 'FiftyWords',
-            'Fish', 'FordA', 'FordB', 'GunPoint', 'Ham', 'HandOutlines', 'Haptics', 'Herring', 'InlineSkate',
-            'InsectWingbeatSound', 'ItalyPowerDemand', 'LargeKitchenAppliances', 'Lightning2', 'Lightning7',
-            'Mallat', 'Meat', 'MedicalImages', 'MiddlePhalanxOutlineCorrect', 'MiddlePhalanxOutlineAgeGroup',
-            'MiddlePhalanxTW', 'MoteStrain', 'NonInvasiveFatalECGThorax1', 'NonInvasiveFatalECGThorax2', 'OliveOil',
-            'OSULeaf', 'PhalangesOutlinesCorrect', 'Phoneme', 'Plane', 'ProximalPhalanxOutlineCorrect',
-            'ProximalPhalanxOutlineAgeGroup', 'ProximalPhalanxTW', 'RefrigerationDevices', 'ScreenType', 'ShapeletSim',
-            'ShapesAll', 'SmallKitchenAppliances', 'SonyAIBORobotSurface1', 'SonyAIBORobotSurface2', 'StarLightCurves',
-            'Strawberry', 'SwedishLeaf', 'Symbols', 'SyntheticControl', 'ToeSegmentation1', 'ToeSegmentation2',
-            'Trace', 'TwoLeadECG', 'TwoPatterns', 'UWaveGestureLibraryX', 'UWaveGestureLibraryY',
-            'UWaveGestureLibraryZ', 'UWaveGestureLibraryAll', 'Wafer', 'Wine', 'WordSynonyms', 'Worms', 'WormsTwoClass',
-            'Yoga', 'ArticularyWordRecognition', 'AtrialFibrillation', 'BasicMotions', 'CharacterTrajectories', 'Cricket',
-            'DuckDuckGeese', 'EigenWorms', 'Epilepsy', 'EthanolConcentration', 'ERing', 'FaceDetection', 'FingerMovements',
-            'HandMovementDirection', 'Handwriting', 'Heartbeat', 'InsectWingbeat', 'JapaneseVowels', 'Libras', 'LSST',
-            'MotorImagery', 'NATOPS', 'PenDigits', 'PEMS-SF', 'Phoneme', 'RacketSports', 'SelfRegulationSCP1',
-            'SelfRegulationSCP2', 'SpokenArabicDigits', 'StandWalkJump', 'UWaveGestureLibrary']
+# datasets = ['Adiac', 'ArrowHead', 'Beef', 'BeetleFly', 'BirdChicken', 'Car', 'CBF', 'ChlorineConcentration',
+            # 'CinCECGTorso', 'Coffee', 'Computers', 'CricketX', 'CricketY', 'CricketZ', 'DiatomSizeReduction',
+            # 'DistalPhalanxOutlineCorrect', 'DistalPhalanxOutlineAgeGroup', 'DistalPhalanxTW', 'Earthquakes',
+            # 'ECG200', 'ECG5000', 'ECGFiveDays', 'ElectricDevices', 'FaceAll', 'FaceFour', 'FacesUCR', 'FiftyWords',
+            # 'Fish', 'FordA', 'FordB', 'GunPoint', 'Ham', 'HandOutlines', 'Haptics', 'Herring', 'InlineSkate',
+            # 'InsectWingbeatSound', 'ItalyPowerDemand', 'LargeKitchenAppliances', 'Lightning2', 'Lightning7',
+            # 'Mallat', 'Meat', 'MedicalImages', 'MiddlePhalanxOutlineCorrect', 'MiddlePhalanxOutlineAgeGroup',
+            # 'MiddlePhalanxTW', 'MoteStrain', 'NonInvasiveFatalECGThorax1', 'NonInvasiveFatalECGThorax2', 'OliveOil',
+            # 'OSULeaf', 'PhalangesOutlinesCorrect', 'Phoneme', 'Plane', 'ProximalPhalanxOutlineCorrect',
+            # 'ProximalPhalanxOutlineAgeGroup', 'ProximalPhalanxTW', 'RefrigerationDevices', 'ScreenType', 'ShapeletSim',
+            # 'ShapesAll', 'SmallKitchenAppliances', 'SonyAIBORobotSurface1', 'SonyAIBORobotSurface2', 'StarLightCurves',
+            # 'Strawberry', 'SwedishLeaf', 'Symbols', 'SyntheticControl', 'ToeSegmentation1', 'ToeSegmentation2',
+            # 'Trace', 'TwoLeadECG', 'TwoPatterns', 'UWaveGestureLibraryX', 'UWaveGestureLibraryY',
+            # 'UWaveGestureLibraryZ', 'UWaveGestureLibraryAll', 'Wafer', 'Wine', 'WordSynonyms', 'Worms', 'WormsTwoClass',
+            # 'Yoga', 'ArticularyWordRecognition', 'AtrialFibrillation', 'BasicMotions', 'CharacterTrajectories', 'Cricket',
+            # 'DuckDuckGeese', 'EigenWorms', 'Epilepsy', 'EthanolConcentration', 'ERing', 'FaceDetection', 'FingerMovements',
+            # 'HandMovementDirection', 'Handwriting', 'Heartbeat', 'InsectWingbeat', 'JapaneseVowels', 'Libras', 'LSST',
+            # 'MotorImagery', 'NATOPS', 'PenDigits', 'PEMS-SF', 'Phoneme', 'RacketSports', 'SelfRegulationSCP1',
+            # 'SelfRegulationSCP2', 'SpokenArabicDigits', 'StandWalkJump', 'UWaveGestureLibrary']
+
+datasets = ['ArticularyWordRecognition', 'BasicMotions', 'CinCECGTorso', 'Cricket',
+     'EigenWorms', 'Epilepsy', 'EthanolConcentration', 'FaceDetection',
+     'FingerMovements', 'HandMovementDirection', 'Handwriting', 'Heartbeat', 'Libras',
+     'LSST', 'MotorImagery', 'NATOPS', 'NonInvasiveFatalECGThorax1',
+     'NonInvasiveFatalECGThorax2', 'PEMS-SF', 'PenDigits', 'RacketSports',
+     'SelfRegulationSCP1', 'SelfRegulationSCP2', 'StandWalkJump', 'StarLightCurves',
+     'UWaveGestureLibrary']
+
+# datasets_grouped = [
+#     ['ArticularyWordRecognition', 'BasicMotions', 'CinCECGTorso', 'Cricket',
+#      'EigenWorms', 'Epilepsy', 'EthanolConcentration', 'FaceDetection',
+#      'FingerMovements', 'HandMovementDirection', 'Handwriting', 'Heartbeat', 'Libras'],
+#
+#     ['LSST', 'MotorImagery', 'NATOPS', 'NonInvasiveFatalECGThorax1',
+#      'NonInvasiveFatalECGThorax2', 'PEMS-SF', 'PenDigits', 'RacketSports',
+#      'SelfRegulationSCP1', 'SelfRegulationSCP2', 'StandWalkJump', 'StarLightCurves',
+#      'UWaveGestureLibrary']
+# ]
+
+# ['SmoothSubspace', 'Chinatown', 'ItalyPowerDemand', 'MelbournePedestrian', 'Crop', 'SyntheticControl', 'SonyAIBORobotSurface2',
+#   'SonyAIBORobotSurface1', 'DistalPhalanxOutlineAgeGroup', 'DistalPhalanxOutlineCorrect', 'DistalPhalanxTW', 'MiddlePhalanxOutlineAgeGroup',
+#     'MiddlePhalanxOutlineCorrect', 'MiddlePhalanxTW', 'PhalangesOutlinesCorrect', 'ProximalPhalanxOutlineAgeGroup', 'ProximalPhalanxOutlineCorrect',
+#       'ProximalPhalanxTW', 'TwoLeadECG', 'MoteStrain', 'ECG200', 'ElectricDevices', 'MedicalImages', 'BME', 'CBF', 'SwedishLeaf', 'TwoPatterns',
+#         'FaceAll', 'FacesUCR', 'ECGFiveDays', 'ECG5000', 'Plane', 'PowerCons', 'GunPoint', 'GunPointAgeSpan', 'GunPointMaleVersusFemale',
+#           'GunPointOldVersusYoung', 'UMD', 'Wafer', 'ChlorineConcentration', 'Adiac', 'Fungi', 'Wine', 'Strawberry', 'ArrowHead', 'InsectWingbeatSound',
+#             'FiftyWords', 'WordSynonyms', 'Trace', 'ToeSegmentation1', 'Coffee', 'DodgerLoopDay', 'DodgerLoopGame', 'DodgerLoopWeekend', 'CricketX',
+#               'CricketY', 'CricketZ', 'FreezerRegularTrain', 'FreezerSmallTrain', 'UWaveGestureLibraryX', 'UWaveGestureLibraryY', 'UWaveGestureLibraryZ',
+#                 'Lightning7', 'ToeSegmentation2', 'DiatomSizeReduction', 'FaceFour', 'GestureMidAirD1', 'GestureMidAirD2', 'GestureMidAirD3',
+#                   'PickupGestureWiimoteZ', 'AllGestureWiimoteX', 'AllGestureWiimoteY', 'AllGestureWiimoteZ', 'ShakeGestureWiimoteZ', 'Symbols', 'Yoga',
+#                     'OSULeaf', 'Ham', 'Meat', 'GesturePebbleZ1', 'GesturePebbleZ2', 'Fish', 'Beef', 'FordA', 'FordB', 'ShapeletSim', 'BeetleFly',
+#                       'BirdChicken', 'Earthquakes', 'Herring', 'ShapesAll', 'OliveOil', 'Car', 'InsectEPGRegularTrain', 'InsectEPGSmallTrain',
+#                         'Lightning2', 'Computers', 'LargeKitchenAppliances', 'RefrigerationDevices', 'ScreenType', 'SmallKitchenAppliances',
+#                           'NonInvasiveFetalECGThorax1', 'NonInvasiveFetalECGThorax2', 'Worms', 'WormsTwoClass', 'UWaveGestureLibraryAll', 'Mallat',
+#                             'MixedShapesSmallTrain', 'Phoneme', 'StarlightCurves', 'Haptics', 'EOGHorizontalSignal', 'EOGVerticalSignal', 'PLAID',
+#                               'ACSF1', 'SemgHandGenderCh2', 'SemgHandMovementCh2', 'SemgHandSubjectCh2', 'CinCECGtorso', 'EthanolLevel', 'InlineSkate',
+#                                 'HouseTwenty', 'PigAirwayPressure', 'PigArtPressure', 'PigCVP', 'HandOutlines', 'Rock']
 
 # datasets = ['Adiac', 'ArrowHead', 'Beef', 'BeetleFly', 'BirdChicken', 'Car', 'CBF', 'ChlorineConcentration']
 
 # datasets = ['Adiac', 'ArrowHead', 'Beef', 'BeetleFly']
 
-N_PARALLEL_DATASETS = 8
+N_PARALLEL_DATASETS = 26
 
 # Split the datasets into N_PARALLEL_DATASETS groups
 dataset_groups = np.array_split(datasets, N_PARALLEL_DATASETS)
@@ -106,25 +143,25 @@ for group in dataset_groups:
                 with open(f"knn_{first_dataset}_{last_dataset}_{k}_{metric}.sbatch", "w") as f:
                     f.write(sbatch_content_knn)
 
-    for metric in distance_metrics:
-        if metric == 'softdtw':
-            for gamma in gamma_values:
-                sbatch_content_ncc = sbatch_template_ncc.format(datasets="_".join(group), metric=metric, gamma=gamma)
-                with open(f"ncc_{first_dataset}_{last_dataset}_{metric}_{gamma}.sbatch", "w") as f:
-                    f.write(sbatch_content_ncc)
-        else:
-            sbatch_content_ncc = sbatch_template_ncc.format(datasets="_".join(group), metric=metric, gamma="")
-            with open(f"ncc_{first_dataset}_{last_dataset}_{metric}.sbatch", "w") as f:
-                f.write(sbatch_content_ncc)
+    # for metric in distance_metrics:
+    #     if metric == 'softdtw':
+    #         for gamma in gamma_values:
+    #             sbatch_content_ncc = sbatch_template_ncc.format(datasets="_".join(group), metric=metric, gamma=gamma)
+    #             with open(f"ncc_{first_dataset}_{last_dataset}_{metric}_{gamma}.sbatch", "w") as f:
+    #                 f.write(sbatch_content_ncc)
+    #     else:
+    #         sbatch_content_ncc = sbatch_template_ncc.format(datasets="_".join(group), metric=metric, gamma="")
+    #         with open(f"ncc_{first_dataset}_{last_dataset}_{metric}.sbatch", "w") as f:
+    #             f.write(sbatch_content_ncc)
 
-    for n_cluster in n_clusters:
-        for metric in distance_metrics:
-            if metric == 'softdtw':
-                for gamma in gamma_values:
-                    sbatch_content_clustering = sbatch_template_clustering.format(datasets="_".join(group), n_cluster=n_cluster, metric=metric, gamma=gamma)
-                    with open(f"clustering_{first_dataset}_{last_dataset}_{n_cluster}_{metric}_{gamma}.sbatch", "w") as f:
-                        f.write(sbatch_content_clustering)
-            else:
-                sbatch_content_clustering = sbatch_template_clustering.format(datasets="_".join(group), n_cluster=n_cluster, metric=metric, gamma="")
-                with open(f"clustering_{first_dataset}_{last_dataset}_{n_cluster}_{metric}.sbatch", "w") as f:
-                    f.write(sbatch_content_clustering)
+    # for n_cluster in n_clusters:
+    #     for metric in distance_metrics:
+    #         if metric == 'softdtw':
+    #             for gamma in gamma_values:
+    #                 sbatch_content_clustering = sbatch_template_clustering.format(datasets="_".join(group), n_cluster=n_cluster, metric=metric, gamma=gamma)
+    #                 with open(f"clustering_{first_dataset}_{last_dataset}_{n_cluster}_{metric}_{gamma}.sbatch", "w") as f:
+    #                     f.write(sbatch_content_clustering)
+    #         else:
+    #             sbatch_content_clustering = sbatch_template_clustering.format(datasets="_".join(group), n_cluster=n_cluster, metric=metric, gamma="")
+    #             with open(f"clustering_{first_dataset}_{last_dataset}_{n_cluster}_{metric}.sbatch", "w") as f:
+    #                 f.write(sbatch_content_clustering)
