96f40a5dd8be8f504b3bc3f949be5675f743b36d
andrey <lavrandge@gmail.com>
2025-02-27 07:56:00 +0200

    cache datasets - all

--- experiments/local_run.py
+++ experiments/local_run.py
@@ -1,7 +1,7 @@
 import subprocess
 import concurrent.futures
 
-datasets = ['EOGVerticalSignal']
+datasets = ['ACSF1']
 k_values = [1, 3, 5]
 distance_metrics = ['euclidean', 'dtw', 'softdtw']
 gamma_values = [0.1, 1, 10]
@@ -13,17 +13,14 @@ def run_experiment(command):
 commands = []
 
 for dataset in datasets:
-    for k in k_values:
-        for metric in distance_metrics:
-            if metric == 'softdtw':
-                for gamma in gamma_values:
-                    commands.append(['python3', 'knn.py', '--dataset', dataset, '--k', str(k), '--metric', metric, '--gamma', str(gamma)])
-            else:
-                commands.append(['python3', 'knn.py', '--dataset', dataset, '--k', str(k), '--metric', metric])
-
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(run_experiment, commands)
-
+    # for k in k_values:
+    #     for metric in distance_metrics:
+    #         if metric == 'softdtw':
+    #             for gamma in gamma_values:
+    #                 commands.append(['python3', 'knn.py', '--dataset', dataset, '--k', str(k), '--metric', metric, '--gamma', str(gamma)])
+    #         else:
+    #             commands.append(['python3', 'knn.py', '--dataset', dataset, '--k', str(k), '--metric', metric])
+    #
     # for metric in distance_metrics:
     #     if metric == 'softdtw':
     #         for gamma in gamma_values:
@@ -32,13 +29,16 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     #     else:
     #         # Run NCC experiment
     #         subprocess.run(['python3', 'ncc.py', '--dataset', dataset, '--metric', metric])
-    #
-    # for n_cluster in n_clusters:
-    #     for metric in distance_metrics:
-    #         if metric == 'softdtw':
-    #             for gamma in gamma_values:
-    #                 # Run Clustering experiment
-    #                 subprocess.run(['python3', 'clustering.py', '--dataset', dataset, '--n_clusters', str(n_cluster), '--metric', metric, '--gamma', str(gamma)])
-    #         else:
-    #             # Run Clustering experiment
-    #             subprocess.run(['python3', 'clustering.py', '--dataset', dataset, '--n_clusters', str(n_cluster), '--metric', metric])
+
+    for n_cluster in n_clusters:
+        for metric in distance_metrics:
+            if metric == 'softdtw':
+                for gamma in gamma_values:
+                    # Run Clustering experiment
+                    subprocess.run(['python3', 'clustering.py', '--dataset', dataset, '--n_clusters', str(n_cluster), '--metric', metric, '--gamma', str(gamma)])
+            else:
+                # Run Clustering experiment
+                subprocess.run(['python3', 'clustering.py', '--dataset', dataset, '--n_clusters', str(n_cluster), '--metric', metric])
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(run_experiment, commands)
